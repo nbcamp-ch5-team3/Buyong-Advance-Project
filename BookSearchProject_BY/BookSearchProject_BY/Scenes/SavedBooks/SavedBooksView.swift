88b7dfc090b88dbca06c26a8e73d7d971bd5b6e1
LEEBUYONG <dqd001@naver.com>
2025-05-09 18:31:23 +0900

    [Setting] #1 - View Defaults Setting

--- BookSearchProject_BY/BookSearchProject_BY/Scenes/SavedBooks/SavedBooksView.swift
+++ BookSearchProject_BY/BookSearchProject_BY/Scenes/SavedBooks/SavedBooksView.swift
@@ -4,3 +4,19 @@
 //
 //  Created by iOS study on 5/9/25.
 //
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SavedBooksView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- BookSearchProject_BY/BookSearchProject_BY/Scenes/Search/SearchView.swift
+++ BookSearchProject_BY/BookSearchProject_BY/Scenes/Search/SearchView.swift
@@ -5,3 +5,18 @@
 //  Created by iOS study on 5/9/25.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
