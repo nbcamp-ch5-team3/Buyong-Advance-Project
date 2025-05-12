d07acbfb7de38acdd7bf0ff5ff1fa35d486beac0
LEEBUYONG <dqd001@naver.com>
2025-05-12 14:43:32 +0900

    [Feat] #3 - SearchViewModel 작성
    REST API로 구현하였으며, Alamofire(헤더 지원)과 RxSwift 사용(books로 결과 전달)하고, 쿼리 파라미터로 검색어를 인코딩하는 방식.

---
+++ BookSearchProject_BY/BookSearchProject_BY/ViewModel/SearchViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  SearchViewModel.swift
+//  BookSearchProject_BY
+//
+//  Created by iOS study on 5/12/25.
+//
+
+import Alamofire
+import RxSwift
+import RxCocoa
+
+class SearchViewModel {
+    private let disposeBag = DisposeBag()
+    private let apiKey = "fd377cd5d584a8c6f60b855dae5e5509"
+    
+    let books = BehaviorSubject(value: [Book]())
+    let error = PublishSubject<Error>()
+    
+    func fetchBooks(query: String) {
+        // 검색어를 URL에 쓸 수 있도록 인코딩
+        guard let queryEncoded = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            error.onNext(NetworkError.invalidUrl)
+            return
+        }
+        
+        let url = "https://dapi.kakao.com/v3/search/book?query=\(queryEncoded)"
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "KaKaoAK \(apiKey)"
+        ]
+        
+        NetworkManager.shared.fetch(url: url, headers: headers)
+            .subscribe(onSuccess: { [weak self] (response: BookResponse) in
+                self?.books.onNext(response.documents)
+            }, onFailure: {[weak self] error in
+                self?.error.onNext(error)
+            }) .disposed(by: disposeBag)
+    }
+}
